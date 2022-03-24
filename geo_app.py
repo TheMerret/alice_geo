4895ca1addfed532a38f1aa25f6909336c369201
TheMerret <merret@bk.ru>
2022-03-24 23:54:47 +0300

    added suing name to app

--- geo_app.py
+++ geo_app.py
@@ -14,6 +14,8 @@ logging.basicConfig(level=logging.INFO, filename='app.log',
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
 
+sessionStorage = {}
+
 @app.route('/post', methods=['POST'])
 def main():
     logging.info('Request: %r', request.json)
@@ -32,23 +34,39 @@ def main():
 def handle_dialog(res, req):
     user_id = req['session']['user_id']
     if req['session']['new']:
+        res['response']['text'] = 'Привет! Назови своё имя!'
+        sessionStorage[user_id] = {
+            'first_name': None,  # здесь будет храниться имя
+            "new": True,
+        }
+        return
+    first_name = sessionStorage[user_id]['first_name']
+    if first_name is None:
+        first_name = get_first_name(req)
+        if first_name is None:
+            res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
+            return
+    if sessionStorage[user_id]['first_name'] is None:
+        sessionStorage[user_id]['first_name'] = first_name
+    if sessionStorage[user_id]["new"]:
         res['response']['text'] = \
-            'Привет! Я могу показать город или сказать расстояние между городами!'
+            f'Привет, {first_name}! Я могу показать город или сказать расстояние между городами!'
+        sessionStorage[user_id].pop("new")
         return
     # Получаем города из нашего
     cities = get_cities(req)
     if not cities:
-        res['response']['text'] = 'Ты не написал название не одного города!'
+        res['response']['text'] = f'{first_name}, ты не написал название не одного города!'
     elif len(cities) == 1:
-        res['response']['text'] = 'Этот город в стране - ' + \
+        res['response']['text'] = f'{first_name}, этот город в стране - ' + \
                                   get_country(cities[0])
     elif len(cities) == 2:
         distance = get_distance(get_coordinates(
             cities[0]), get_coordinates(cities[1]))
-        res['response']['text'] = 'Расстояние между этими городами: ' + \
+        res['response']['text'] = f'{first_name}, расстояние между этими городами: ' + \
                                   str(round(distance)) + ' км.'
     else:
-        res['response']['text'] = 'Слишком много городов!'
+        res['response']['text'] = f'{first_name}, слишком много городов!'
 
 
 def get_cities(req):
@@ -58,6 +76,16 @@ def get_cities(req):
             if 'city' in entity['value']:
                 cities.append(entity['value']['city'])
     return cities
+
+
+def get_first_name(req):
+    # перебираем сущности
+    for entity in req['request']['nlu']['entities']:
+        # находим сущность с типом 'YANDEX.FIO'
+        if entity['type'] == 'YANDEX.FIO':
+            # Если есть сущность с ключом 'first_name', то возвращаем её значение.
+            # Во всех остальных случаях возвращаем None.
+            return entity['value'].get('first_name', None)
 
 
 if __name__ == '__main__':
